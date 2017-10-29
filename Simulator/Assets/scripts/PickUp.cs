20cab0267430fa4a5aaf23215e8e075300383911
AdityaGupta1 <adityag@gmail.com>
2017-10-29 00:03:27 -0700

    changed from third-person to first-person, added log that can be picked up, closed #1 (because "ethan" doesn't exist anymore)

---
+++ Simulator/Assets/scripts/PickUp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour 
+{
+	public Transform target;
+	Rigidbody body;
+
+	void Start()
+	{
+		body = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void OnMouseDown()
+	{
+		this.transform.position = target.position;
+		this.transform.parent = GameObject.Find("FPSController").transform;
+		this.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+		body.isKinematic = true;
+	}
+
+	void OnMouseUp()
+	{
+		this.transform.parent = GameObject.Find("FPSController").transform;
+		this.transform.parent = null;
+		body.isKinematic = false;
+	}
+
+}﻿
