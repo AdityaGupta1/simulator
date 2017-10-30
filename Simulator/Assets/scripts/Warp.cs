27cdf05347c17d3ddd32f5ca4ce37d8c88b03ece
AdityaGupta1 <adityag@gmail.com>
2017-10-29 21:52:25 -0700

    added warps, removed light from coins

---
+++ Simulator/Assets/scripts/Warp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Warp Start") {
+			transform.position = other.GetComponent<WarpTarget>().target.position;
+		}
+	}﻿
+}
